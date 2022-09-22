f60dbdf4fa2e9ee62dcc9020db6cc499469db7c0
Sarinke John <67694281+sarinkejohn@users.noreply.github.com>
2022-09-22 15:44:21 +0300

    first commit

---
+++ amashikolo/frontend/src/components/Product.js
@@ -0,0 +1,64 @@
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
+import Rating from './Rating';
+import axios from 'axios';
+import { useContext } from 'react';
+import { Store } from '../Store';
+
+function Product(props) {
+  const { product } = props;
+
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const {
+    cart: { cartItems },
+  } = state;
+
+  const addToCartHandler = async (item) => {
+    const existItem = cartItems.find((x) => x._id === product._id);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get(`/api/products/${item._id}`);
+    if (data.countInStock < quantity) {
+      window.alert('Sorry. Product is out of stock');
+      return;
+    }
+    ctxDispatch({
+      type: 'CART_ADD_ITEM',
+      payload: { ...item, quantity },
+    });
+  };
+  const mystyle = {
+    backgroundColor: 'white',
+    padding: '20px',
+    textAlign: 'center',
+    boxShadow: 'rgb(3 0 71 /9%) 0px 1px 3px',
+    margin: '10px',
+    borderRadius: '8px',
+  };
+  return (
+    <Card style={mystyle}>
+      <Link to={`/product/${product.slug}`}>
+        <img
+          src={product.image}
+          className="card-img-top shrink-0"
+          alt={product.name}
+        />
+      </Link>
+      <Card.Body style={{}}>
+        <Link to={`/product/${product.slug}`}>
+          <Card.Title>{product.name}</Card.Title>
+        </Link>
+        <Rating rating={product.rating} numReviews={product.numReviews} />
+        <Card.Text>${product.price}</Card.Text>
+        {product.countInStock === 0 ? (
+          <Button variant="light" disabled>
+            Out of stock
+          </Button>
+        ) : (
+          <Button onClick={() => addToCartHandler(product)}>Add to cart</Button>
+        )}
+      </Card.Body>
+    </Card>
+  );
+}
+export default Product;
